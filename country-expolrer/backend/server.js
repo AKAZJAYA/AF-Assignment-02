0503e5746d1fe6132aa8c1b15d2bb7a7a2001c33
AKAZJAYA <savindu096@gmail.com>
2025-04-21 12:51:58 +0530

    fix: Update CORS configuration to allow requests from any origin refactor: Remove unused custom animations from index.css

--- country-expolrer/backend/server.js
+++ country-expolrer/backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your frontend URL
+    origin: "*", // Your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "x-auth-token"],
   })
